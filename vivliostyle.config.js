0ea56efd7fab18c98706b0c85847363c57c8fa23
Ryoga.exe <ryoga.dev@gmail.com>
2022-04-23 16:45:12 +0900

    add: vivliostyle project

---
+++ vivliostyle.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: 'book-2022',
+  author: 'kss-pc-club',
+  language: 'ja',
+  size: 'JIS-B5',
+  theme: 'css/theme_print.css',
+  entryContext: './manuscripts',
+  entry: [
+    { rel: 'contents' },
+    'foreword.md',
+    'sample/index.md',
+  ],
+  output: [
+    './public/output.pdf',
+  ],
+  toc: true,
+  tocTitle: '目次',
+  workspaceDir: '.cache',
+  // cover: './cover.png', // cover image. default to undefined.
+  // vfm: { // options of VFM processor
+  //   hardLineBreaks: true, // converts line breaks of VFM to <br> tags. default to 'false'.
+  //   disableFormatHtml: true, // disables HTML formatting. default to 'false'.
+  // },
+}
